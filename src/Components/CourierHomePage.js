0c1fa7018491bab292aad6110ce418c2bed6781b
Bilgehan Akcan <bilgehan.akcan@zegteknoloji.com>
2021-11-28 17:19:47 +0300

    courier home page

--- src/Components/CourierHomePage.js
+++ src/Components/CourierHomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "./NavBar";
+import PackageAcceptance from "./PackageAcceptance";
 
 function CourierHomePage() {
     return (
         <div>
             <NavBar></NavBar>
-            
+            <PackageAcceptance></PackageAcceptance>
         </div>
     );
 }
