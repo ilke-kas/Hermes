a7954fa475ae950c20274125dcc0f9d1f29bf669
İlke Kaş <ilkeekas@gmail.com>
2021-12-10 23:17:30 +0300

    commit 3
    -login ve signup çalışıyor.

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -3,24 +3,233 @@ import React, { useState, useEffect } from 'react';
 import Logo from '../Logo';
 
 function SignUp() {
-    const [selected, setSelected] = useState('');
-    const [selected2, setSelected2] = useState('');
+    const [userid, setUserId] = React.useState("");
+    const [name, setName] = React.useState("");
+    const [phone, setPhone] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
-    const selectionChangeHandler = (event) => {
-      setSelected(event.target.value);
-    };
+    //customer things
+    const [street, setStreet] = React.useState("");
+    const [aptnumber, setAptNumber] = React.useState("");
+    const [city, setCity] = React.useState("");
+    const [state, setState] = React.useState("");
+    const [zip, setZip] = React.useState("");
+    const [selectedCustomer, setSelectedCustomer] = useState('');
+    const [selectedEmployee, setSelectedEmployee] = useState('');
+    const [selected, setSelected] = useState('');
+    const [trigger, setTrigger] = React.useState(false);
+    const [text, setText] = React.useState("")
+
+    //employee things
+    const [branchid, setBranchId] = React.useState("");
+    const [vehicleid, setVehicleId] = React.useState("");
+
+
+    async function register(e){
+      console.log("selected :" + selected);
+      console.log("customer :" + selectedCustomer);
+      console.log("employee :" + selectedEmployee);
+      if ( userid != "" && name != "" && email != "" && password != "" ) {
+            if(selected == ""){
+              const body = {userid,email,name,phone,password,street,aptnumber,city,state,zip};
+              var temp;
+              var temp2;
+              if(selectedCustomer == "Individual"){
+                const response = await fetch("http://localhost:3001/registerIndividual", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+                }).then(x => x.json())
+                .then(data => {
+                  temp = data.isValid;
+                  temp2 = data.type;
+                  if(temp){
+                    window.location = '/';
+                  }
+                  else{
+                    if(temp2 == "userid"){
+                      alert("This user id is already taken.Please select another one.");
+                    }
+                    else if(temp2 == "email"){
+                      alert("This email is already taken.Please select another one.");
+                    }
+                    else if(temp2 == "phone"){
+                      alert("This phone number is already taken.Please select another one.");
+                    }
+                    else if(temp2 == "branchid"){
+                      alert("Please write valid branch id.");
+                    }
+                    else if(temp2 == "vehicleid"){
+                      alert("Please write valid vehicle id.");
+                    }
+                  }
+                });
+              }
+              else if(selectedCustomer == "Corporate"){
+                const response = await fetch("http://localhost:3001/registerCorporate", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(body)
+                  }).then(x => x.json())
+                  .then(data => {
+                    temp = data.isValid;
+                    temp2 = data.type;
+                    if(temp){
+                      window.location = '/';
+                    }
+                    else{
+                      if(temp2 == "userid"){
+                        alert("This user id is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "email"){
+                        alert("This email is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "phone"){
+                        alert("This phone number is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "branchid"){
+                        alert("Please write valid branch id.");
+                      }
+                      else if(temp2 == "vehicleid"){
+                        alert("Please write valid vehicle id.");
+                      }
+                    }
+                  });
+              }
+              else{
+                setText("Please choose type of the customer!");
+              }
+            }
+            else if(selected == "false"){
+              if(selectedEmployee == "PackageManager"){
+                const body = {userid,email,name,phone,password,branchid};
+                const response = await fetch("http://localhost:3001/registerPackageManager", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(body)
+                  }).then(x => x.json())
+                  .then(data => {
+                    temp = data.isValid;
+                    temp2 = data.type;
+                    if(temp){
+                      window.location = '/';
+                    }
+                    else{
+                      if(temp2 == "userid"){
+                        alert("This user id is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "email"){
+                        alert("This email is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "phone"){
+                        alert("This phone number is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "branchid"){
+                        alert("Please write valid branch id.");
+                      }
+                      else if(temp2 == "vehicleid"){
+                        alert("Please write valid vehicle id.");
+                      }
+                    }
+                  });
+
+              }
+              else if(selectedEmployee == "Shipper"){
+                const body = {userid,email,name,phone,password,vehicleid};
+                const response = await fetch("http://localhost:3001/registerShipper", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(body)
+                  }).then(x => x.json())
+                  .then(data => {
+                    temp = data.isValid;
+                    temp2 = data.type;
+                    if(temp){
+                      window.location = '/';
+                    }
+                    else{
+                      if(temp2 == "userid"){
+                        alert("This user id is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "email"){
+                        alert("This email is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "phone"){
+                        alert("This phone number is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "branchid"){
+                        alert("Please write valid branch id.");
+                      }
+                      else if(temp2 == "vehicleid"){
+                        alert("Please write valid vehicle id.");
+                      }
+                    }
+                  });
+
+              }
+              else if(selectedEmployee == "Courier"){
+                const body = {userid,email,name,phone,password,vehicleid,branchid};
+                const response = await fetch("http://localhost:3001/registerCourier", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(body)
+                  }).then(x => x.json())
+                  .then(data => {
+                    temp = data.isValid;
+                    temp2 = data.type;
+                    if(temp){
+                      window.location = '/';
+                    }
+                    else{
+                      if(temp2 == "userid"){
+                        alert("This user id is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "email"){
+                        alert("This email is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "phone"){
+                        alert("This phone number is already taken.Please select another one.");
+                      }
+                      else if(temp2 == "branchid"){
+                        alert("Please write valid branch id.");
+                      }
+                      else if(temp2 == "vehicleid"){
+                        alert("Please write valid vehicle id.");
+                      }
+                    }
+                  });
+
+              }
+              else{
+                //please chooose one error
+                setText("Please choose type of the employee!");
+              }
+
+            }
+            else{
+              //please choose one error
+              setText("Please choose type of the user!");
+            }
+    }
+    else {
+        setText("Please enter valid values for user name, email and password!");
+        setTrigger(true);
+    }
+}
    
     function employee() {
       return (
         <div>
           {/* <TextField className="signuptextfield mt-3" id="branch-id" label="Branch ID" variant="outlined"/> */}
-          <RadioGroup className="d-flex justify-content-center mt-3" row aria-label="employeekind" defaultValue="customer" name="row-radio-buttons-group" value={selected2} onChange={event => setSelected2(event.target.value)}> 
+          <RadioGroup className="d-flex justify-content-center mt-3" row aria-label="employeekind" defaultValue="customer" name="row-radio-buttons-group" value={selectedEmployee} onChange={event => setSelectedEmployee(event.target.value)}> 
             <FormControlLabel value="Shipper" control={<Radio />} label="Shipper" />
             <FormControlLabel value="PackageManager" control={<Radio />} label="PackageManager" />
             <FormControlLabel value="Courier" control={<Radio />} label="Courier" />
           </RadioGroup>
-          {selected2 == "PackageManager" ? <TextField className="signuptextfield" id="branch-id" label="Branch ID" variant="outlined"/> : 
-          <TextField className="signuptextfield" id="branch-id" label="Vehicle ID" variant="outlined"/>}
+          {selectedEmployee == "PackageManager" ? <TextField className="signuptextfield" onChange={e => setBranchId(e.target.value)} id="branchid" label="Branch ID" variant="outlined"/> : (selectedEmployee == "Courier" ?
+          <div><TextField className="signuptextfield" onChange={e => setVehicleId(e.target.value)} id="vehicleid" label="Vehicle ID" variant="outlined"/><br></br><br></br><TextField className="signuptextfield" onChange={e => setBranchId(e.target.value)} id="branchid" label="Branch ID" variant="outlined"/></div>:
+          <TextField className="signuptextfield" onChange={e => setVehicleId(e.target.value)} id="vehicleid" label="Vehicle ID" variant="outlined"/> )}
           <br></br><br></br>
         </div>
       );
@@ -29,15 +238,15 @@ function SignUp() {
       return ( 
         <div>
           <div style={{width:"90%"}} className="input-group">
-            <TextField className="form-control mt-3" id="street" label="Street" variant="outlined"/>
-            <TextField className="form-control mt-3" id="apt-number" label="Apt Number" variant="outlined"/>
+            <TextField className="form-control mt-3" onChange={e => setStreet(e.target.value)} id="street" label="Street" variant="outlined"/>
+            <TextField className="form-control mt-3" onChange={e => setAptNumber(e.target.value)} id="aptnumber" label="Apt Number" variant="outlined"/>
           </div>
           <div style={{width:"90%"}} className="input-group mt-3">
-            <TextField className="form-control mt-3" id="city" label="City" variant="outlined"/>
-            <TextField className="form-control mt-3" id="state" label="State" variant="outlined"/>
-            <TextField className="form-control mt-3" id="zip" label="Zip" variant="outlined"/>
+            <TextField className="form-control mt-3" onChange={e => setCity(e.target.value)} id="city" label="City" variant="outlined"/>
+            <TextField className="form-control mt-3" onChange={e => setState(e.target.value)} id="state" label="State" variant="outlined"/>
+            <TextField className="form-control mt-3" onChange={e => setZip(e.target.value)} id="zip" label="Zip" variant="outlined"/>
           </div>
-          <RadioGroup className="mt-4 d-flex justify-content-center" row aria-label="employeekind" defaultValue="customer" name="row-radio-buttons-group"> 
+          <RadioGroup className="mt-4 d-flex justify-content-center" row aria-label="employeekind" defaultValue="customer" name="row-radio-buttons-group" value={selectedCustomer} onChange={event => setSelectedCustomer(event.target.value)}> 
             <FormControlLabel value="Corporate" control={<Radio />} label="Corporate" />
             <FormControlLabel value="Individual" control={<Radio />} label="Individual" />
           </RadioGroup>
@@ -53,18 +262,18 @@ function SignUp() {
         <center>
           <h2 className="display-5 login-header">Register to Hermes</h2>
 
-          <RadioGroup className="mt-3 d-flex justify-content-center" row aria-label="userkind" defaultValue="true" name="row-radio-buttons-group" value={selected} onChange={selectionChangeHandler}>           
+          <RadioGroup className="mt-3 d-flex justify-content-center" row aria-label="userkind" defaultValue="true" name="row-radio-buttons-group" value={selected} onChange={event => setSelected(event.target.value)}>          
             <FormControlLabel value="" control={<Radio />} label="Customer" />
             <FormControlLabel value="false" control={<Radio />}  label="Employee" />
           </RadioGroup>
           
-          <TextField className="signuptextfield" id="userid" label="User ID" variant="outlined"/>
-          <TextField className="signuptextfield mt-3" id="email" label="Email" variant="outlined"/>
-          <TextField className="signuptextfield mt-3" id="name" label="Name" variant="outlined"/>
-          <TextField className="signuptextfield mt-3" id="phone" label="Phone Number" variant="outlined"/>
-          <TextField className="signuptextfield mt-3" type="password" id="password" label="Password" variant="outlined"/>
+          <TextField className="signuptextfield" onChange={e => setUserId(e.target.value)} id="userid" label="User ID" variant="outlined"/>
+          <TextField className="signuptextfield mt-3" onChange={e => setEmail(e.target.value)} id="email" label="Email" variant="outlined"/>
+          <TextField className="signuptextfield mt-3"  onChange={e => setName(e.target.value)} id="name" label="Name" variant="outlined"/>
+          <TextField className="signuptextfield mt-3"  onChange={e => setPhone(e.target.value)} id="phone" label="Phone Number" variant="outlined"/>
+          <TextField className="signuptextfield mt-3" onChange={e => setPassword(e.target.value)} type="password" id="password" label="Password" variant="outlined"/>
           {selected ?  employee() :customer() }
-          <button type="button" className="btn btn-secondary btn-lg login-button">Sign Up</button><br/><br/>
+          <button type="button" onClick={register} className="btn btn-secondary btn-lg login-button">Sign Up</button><br/><br/>
         </center>
       </div>
     </div>
