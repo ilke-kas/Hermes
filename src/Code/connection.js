748c3b9101a230c46bf356c838b628bf4bbc7532
İlke Kaş <ilkeekas@gmail.com>
2021-12-31 00:06:40 +0300

    new connection
    new connection is established

--- src/Code/connection.js
+++ src/Code/connection.js
@@ -4,7 +4,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool( {
     user: "postgres",
     password: "hermes",
-    host: "104.154.241.26",
+    host: "104.154.130.132",
     port: 5432,
     database: "postgres"
 });
